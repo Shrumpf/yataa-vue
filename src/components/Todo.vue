b58d2d4a62d0c9d9027450e67a7321d3e28b96a6
Shrumpf <flori.voss@gmx.net>
2020-07-18 11:15:25 +0200

    Todo: Added more properties, implemented check todo

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,14 +2,27 @@
   <div>
     <h1>{{ todo.Title }}</h1>
     <p>{{ todo.Content }}</p>
+    <p>{{todo.Username}}</p>
+    <p>{{todo.Created}}</p>
+    <p>{{todo.Updated}}</p>
+    <input type="checkbox" name="done" id="done" :checked="todo.Done" @click="changeTodoState">
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   props: {
     todo: Object
-  }
+  },
+  methods: {
+    changeTodoState: function() {
+      axios.put(`http://localhost:3000/todos/${this.todo.Id}/done`).then(response => {
+      console.log(response);
+    });
+    }
+  },
 };
 </script>
 
